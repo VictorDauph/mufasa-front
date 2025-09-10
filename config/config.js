ffea220849d0fcb2405be87e8de2425bbbb9d45c
VictorDauph <79511345+VictorDauph@users.noreply.github.com>
2025-09-10 15:06:52 +0200

    gestion des urls back différents selon environnements

--- config/config.js
+++ config/config.js
@@ -1,6 +1,13 @@
 export async function loadConfig() {
     const resp = await fetch("/config/config.json");
     const cfg = await resp.json();
-    let API_URL = cfg.api_url;
+
+    // Détection de l'environnement
+    let API_URL;
+    if (window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost") {
+        API_URL = cfg.api_url; // local
+    } else {
+        API_URL = cfg.api_url2; // pages ou prod
+    }
     return API_URL;
 }
